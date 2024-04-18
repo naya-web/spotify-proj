3b9a697b3fb0f8d64d8903dfadf35259c4f1a1bd
Ahmad <ahmadzer607@gmail.com>
2024-04-18 15:53:53 +0300

    added function top 100 album

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import UserInfo from './Pages/UserInfo.jsx';
 import MoreSongs from './Pages/MoreSongs.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Routes, Route } from "react-router-dom";
+import dataSpotify from "./spotify_data.history.json"
 import ReactDOM from 'react-dom';
+import TopAlbums from './Components/filter top 100 album/filterToopalbum.js';
 
 
 function App() {
+  // -----------------------
+  
+  const O_TopAlbums = TopAlbums()
+
+  // ----------------------
   return (
     <>
       <h1>hi im ahmad</h1>
       <h2>hi</h2>
+      <div>{O_TopAlbums}</div>
 
     {/* <HomePage/> */}
     <Routes>
---
+++ src/Components/filter top 100 album/filterToopalbum.js
@@ -0,0 +1,47 @@
+ import dataSpotify from '../../spotify_data.history.json'
+ 
+  function TopAlbums() {
+    const data = dataSpotify;
+
+    // إنشاء كائن لتجميع بيانات الألبومات
+    const albumMap = {};
+
+    // حساب وقت الاستماع لكل ألبوم
+    data.forEach((track) => {
+      const albumName = track.master_metadata_album_album_name;
+      const msPlayed = track.ms_played;
+
+      if (!albumMap[albumName]) {
+        albumMap[albumName] = 0;
+      }
+
+      albumMap[albumName] += msPlayed;
+    });
+
+    // تحويل الكائن إلى مصفوفة من الألبومات
+    const albums = Object.keys(albumMap).map((albumName) => ({
+      albumName,
+      msPlayed: albumMap[albumName],
+    }));
+
+    // فرز الألبومات بناءً على وقت الاستماع
+    const sortedAlbums = albums.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 10);
+    
+    return (
+      <div>
+        <h2>أعلى 10 ألبومات حسب وقت الاستماع</h2>
+        <ul>
+          {sortedAlbums.map((album, index) => (
+            <li key={album.albumName}>
+              {index + 1}. {album.albumName}
+
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+
+  
+  export default TopAlbums
