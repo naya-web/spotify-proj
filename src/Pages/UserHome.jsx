e962bea17eb0cdff3208a7ea6860acbf19b0a401
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2024-04-20 22:45:15 +0300

    #noura_user_home

--- src/Pages/UserHome.jsx
+++ src/Pages/UserHome.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import SideBar from '../Components/SideBar/SideBar'
 import NavBar from '../Components/NavBar/NavBar'
-import Content from '../Components/HomeContent/Content'
 import Footer from '../Components/Footer/Footer'
+import CarouselCardUser from '../Components/HomeContent/CarouselCardUser.jsx';
+import dataSpotify from '../spotify_data.history.json';
+import { Link } from 'react-router-dom'
 
 function UserHome() {
   return (
     <>
     <div className="container">
-      <div className='row mx-0 py-4'>
+      <div className='row mx-0 pt-4'>
         <div className="col-3">
           <SideBar path={'/userInfo'} info={'UserInfo'}/>
         </div>
@@ -16,11 +18,16 @@ function UserHome() {
           <NavBar path={'/'}/>
           <div className='text-end mt-3 fs-2'>
           </div>
-            <Content path={'/moreSongs'}/>
+          <CarouselCardUser data={dataSpotify}/>
         </div>
       </div>
-    </div>
-    <Footer/>
+      <div className='d-flex justify-content-end'>
+            <Link to={'/moreSongsArtist'}>
+                <button className='p-3 more'>Show More</button>
+            </Link>
+        </div>
+      <Footer />
+      </div>
     </>
   )
 }
