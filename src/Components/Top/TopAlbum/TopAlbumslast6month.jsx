4da553853122a9591055764deab887139d197fb8
Ahmad <ahmadzer607@gmail.com>
2024-04-21 18:50:53 +0300

    add css toop 100 albums

--- src/Components/Top/TopAlbum/TopAlbumslast6month.jsx
+++ src/Components/Top/TopAlbum/TopAlbumslast6month.jsx
@@ -1,45 +1,66 @@
 import dataSpotify from '../../../spotify_data.history.json'
 import { useEffect } from "react";
+import  './sc.css'
+
 
 function TopAlbumslast6month() {
-    useEffect(() => {
-      // حساب التاريخ قبل ستة أشهر من الوقت الحالي
-      const sixMonthsAgo = new Date();
-      sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
-  
-    
-      const albumTimes = {};
-      dataSpotify.forEach((song) => {
-        const ts = new Date(song.ts);
-        if (ts >= sixMonthsAgo) {
-          const album = song.master_metadata_album_album_name;
-          const msPlayed = song.ms_played;
-          if (album && album.trim() !== '' && !albumTimes[album]) {
-            albumTimes[album] = msPlayed;
-          }
-        }
-      });
-  
-    
-      const sortedAlbums = Object.entries(albumTimes).sort((a, b) => b[1] - a[1]);
-  
-  
-      const topAlbums = sortedAlbums
-        .slice(0, 100)
-        .map(([album, msPlayed]) => ({ album, msPlayed }));
-  
-     
-      console.log(topAlbums);
-    });
-  
-    return (
-      <div>
-         <h2>Toop 100 Album Last 6 Month</h2>
+  const data = dataSpotify;
 
-        {/* فيكي تعرضيهن بالطريقه يلي بدك */}
-       
-      </div>
-    );
+  // إنشاء كائن لتجميع بيانات الألبومات
+  const albumMap = {};
+
+  // حساب وقت الاستماع لكل ألبوم
+  data.forEach((track) => {
+    const albumName = track.master_metadata_album_album_name;
+    const msPlayed = track.ms_played;
+
+    if (!albumMap[albumName]) {
+      albumMap[albumName] = 0;
+    }
+
+    albumMap[albumName] += msPlayed;
+  });
+
+  // تحويل الكائن إلى مصفوفة من الألبومات
+  const albums = Object.keys(albumMap).map((albumName) => ({
+    albumName,
+    msPlayed: albumMap[albumName],
+  }));
+
+  // فرز الألبومات بناءً على وقت الاستماع
+  const sortedAlbums = albums.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 100);
+
+  return (
+    <div>
+      <h2>Toop 100 Album From Begining</h2>
+      <ul>
+        <div className="container4">
+        {sortedAlbums.map((album, index) => (
+          <li key={album.albumName}>
+           
+            <div class="card">
+        <div class="card-img"></div>
+        <div class="card-info">
+          <p class="text-title">Name:{album.albumName} </p>
+     
+        </div>
+        <div class="card-footer">
+          <span class="text-title">Ms_played:{album.msPlayed}</span>
+         
+          </div>
+        </div>
+          </li>
+        ))}
+        </div>
+      </ul>
+
+
+
+
+
+      
+    </div>
+  );
   }
   // const O_TopAlbumslast6month=TopAlbumslast6month()
 export default TopAlbumslast6month 
